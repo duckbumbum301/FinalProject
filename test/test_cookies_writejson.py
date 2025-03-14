e241529e7aff43da35ff9de99e5e5d4029672037
duckbumbum301 <ducnc234112e@st.uel.edu.vn>
2025-03-14 12:43:45 +0700

    Add files via upload

---
+++ test/test_cookies_writejson.py
@@ -0,0 +1,18 @@
+from libs.JsonFileFactory import JsonFileFactory
+from models.Product import Cookies
+
+cookies=[]
+cookies.append(Cookies("CB19","Classic Choco Chip",35000,0,"","D:/FinalProject/images/classic_choco_chip.png"))
+cookies.append(Cookies("CB20","Dark Mix",40000,0,"","D:/FinalProject/images/dark_mix.png"))
+cookies.append(Cookies("CB21","Cookies&Cream",45000,0,"","D:/FinalProject/images/cookies_cream.png"))
+cookies.append(Cookies("CB22","Creamy Matcha",45000,0,"","D:/FinalProject/images/creamy_matcha.png"))
+cookies.append(Cookies("CB23","Twisted Love",45000,0,"","D:/FinalProject/images/twisted_love.png"))
+cookies.append(Cookies("CB24","Oreo Chocolate",45000,0,"","D:/FinalProject/images/oreo_chocolate.png"))
+print("List of cookies:")
+for c in cookies:
+    print(c)
+
+filename="cookies.json"
+path=f"../dataset/{filename}"
+jff=JsonFileFactory()
+jff.write_data(cookies,path)
