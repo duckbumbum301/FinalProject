70d7ac9f678a63df3e668145f2a8a31d56c2c3d3
duckbumbum301 <ducnc234112e@st.uel.edu.vn>
2025-03-20 20:09:16 +0700

    Add files via upload

--- test/test_login.py
+++ test/test_login.py
@@ -1,10 +1,10 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
-from Ui.LoginExt import LoginMainWindowExt
+from Ui.LoginExt import LoginExt
 
 app = QApplication([])
 mainwindow = QMainWindow()
-ui = LoginMainWindowExt()
+ui = LoginExt()
 ui.setupUi(mainwindow)
 ui.showWindow()
 app.exec()
